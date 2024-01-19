de144fa38adc78fa74e99c9101ad556bbf54eda4
Reuben Remy <reubenremy@gmail.com>
2024-01-19 16:37:46 -0500

    added typeAlias for employee

--- src/employee.ts
+++ src/employee.ts
@@ -11,3 +11,29 @@ let employee: {
         console.log(date)
     }
 }
+
+// Employee 2 Created with type alias
+
+type Employee = {
+    // start of type declaration
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+    // end of type declaration
+}
+
+let employee_two: Employee = {
+    id: 2, 
+    name: 'Rhemidy',
+    retire: (date: Date) => {
+        console.log(date);
+    }
+}
+
+let employee_three: Employee = {
+    id: 3, 
+    name: 'RTM',
+    retire: (date: Date) => {
+        console.log(date);
+    }
+}
